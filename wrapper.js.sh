b3c4143a0e41ddb1c397ff18d621d7f4ffa7648f
Anton Grbin <anton.grbin@gmail.com>
2014-07-14 18:24:46 +0200

    added license on top of compiled file

--- wrapper.js.sh
+++ wrapper.js.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo  "
-var Flann = (function() {
-  var Module = this;
-";
+# output license
+echo  "/*"
+cat LICENSE
+echo  "*/"
+
+# output body
+echo "var Flann = (function() {
+  var Module = this;";
 
 cat bin/flann.js
 
 echo "
   return this;
-}).call({});
-";
+}).call({});";
