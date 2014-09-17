f2c4038ef21b28986348bfe23e2b44c37b75d618
Anton Grbin <anton.grbin@gmail.com>
2014-09-17 23:41:47 +0200

    fix browserify poor shim for process

--- wrapper.js.sh
+++ wrapper.js.sh
@@ -6,11 +6,29 @@ cat LICENSE
 echo  "*/"
 
 # output body
-echo -n "var Flann = (function() {\n
-  var Module = this;";
+cat <<EOF
+// fix browserify poor shim for process.
+if (typeof process !== 'undefined' && !('platform' in process)) {
+  (function () {
+    var write, output;
+    write = write || (typeof console === 'undefined' ? null : console.log);
+    write = write || (typeof print === 'undefined' ? null : print);
+    write = write || function () {};
+    output = { write: write };
+    process.platform = 'maybe-browserify';
+    if (!('stderr' in process)) process.stderr = output;
+    if (!('stdout' in process)) process.stdout = output;
+  }());
+}
 
+var Flann = (function() {
+  var Module = this;
+EOF
+
+# output module code.
 cat bin/flann.js
 
+# output tail
 cat <<EOF
 
   return this;
